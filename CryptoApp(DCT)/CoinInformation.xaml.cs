c73ade0921c79b0b62f71d460ca09f70a85dd0e5
ACER <ACER@DENISKRAVCHENKO>
2022-09-06 08:57:08 +0300

    Добавьте файлы проекта.

---
+++ CryptoApp(DCT)/CoinInformation.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CryptoApp_DCT_
+{
+    /// <summary>
+    /// Логика взаимодействия для CoinInformation.xaml
+    /// </summary>
+    public partial class CoinInformation : Window
+    {
+        public CoinInformation()
+        {
+            InitializeComponent();
+
+            DataContext = new ApplicationViewModel();
+        }
+    }
+}
---
+++ CryptoApp(DCT)/FindCryptoCoin.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CryptoApp_DCT_
+{
+    /// <summary>
+    /// Логика взаимодействия для FindCryptoCoin.xaml
+    /// </summary>
+    public partial class FindCryptoCoin : Window
+    {
+        public FindCryptoCoin()
+        {
+            InitializeComponent();
+
+            DataContext = new ApplicationViewModel();
+        }
+
+        private void MainButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Close();
+        }
+    }
+}
